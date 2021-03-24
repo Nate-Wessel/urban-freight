dbf60cba760c2e4a200001a2d654ff1aef8b92ad
Nate Wessel <bike756@gmail.com>
2021-03-23 20:09:05 -0400

    generalize avoid/shift/improve, create city state

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import Map from './Map'
+import './nav.css'
+
+const cities = [
+	{ 
+		name: 'Toronto',
+		center: [43.65,-79.37]
+	},
+	{
+		name: 'Vancouver',
+		center: [49.26,-123.07]
+	},
+	{
+		name: 'Edmonton',
+		center: [53.53,-113.5]
+	}
+]
+
+export default function(props){
+	const [ city, setCity ] = useState(cities[0])
+	return (
+		<div id="app">
+			<CityNav city={city} setCity={setCity}/>
+			<Map city={city} pradigm={props.paradigm}/>
+		</div>
+	)
+}
+
+function CityNav(props){
+	return (
+		<nav>
+			{ cities.map( (c,i) => {
+				let cls = 'city-link' + (props.city == c ? ' active' : '')
+				function click(e){ props.setCity(cities[i]) }
+				return (
+					<div key={i} className={cls} onClick={click}>
+						{c.name}
+					</div>
+				)
+			} ) }
+		</nav>
+	)
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { render } from 'react-dom';
-import "@babel/polyfill";
-import { Avoid } from './Avoid'
-import { Shift } from './Shift'
-import { Improve } from './Improve'
+import React from 'react'
+import { render } from 'react-dom'
+//import "@babel/polyfill";
+import App from './App'
 
 var reactElem;
-if (reactElem = document.querySelector('#avoid-container'))
-	render(<Avoid/>, reactElem);
-else if (reactElem = document.querySelector('#shift-container'))
-	render(<Shift/>, reactElem);  
-else if (reactElem = document.querySelector('#improve-container'))
-	render(<Improve/>, reactElem);  
+if (reactElem = document.querySelector('#avoid-container')){
+	render(<App paradigm="avoid"/>, reactElem)
+}else if (reactElem = document.querySelector('#shift-container')){
+	render(<App paradigm="shift"/>, reactElem)
+}else if (reactElem = document.querySelector('#improve-container')){
+	render(<App paradigm="improve"/>, reactElem)
+}
