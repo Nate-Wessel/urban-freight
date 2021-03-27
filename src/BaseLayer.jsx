d7d35250397b624d21abdd33a41604c674aed122
Nate Wessel <bike756@gmail.com>
2021-03-27 10:54:44 -0400

    refactor baselayer into subcomponents

--- src/BaseLayer.jsx
+++ src/BaseLayer.jsx
@@ -4,16 +4,6 @@ import { LayerGroup, GeoJSON } from 'react-leaflet'
 import { json } from 'd3-fetch'
 import { scalePow } from 'd3-scale'
 
-const empDensity = scalePow()
-	.exponent(0.5)
-	.domain([0,50000])
-	.range(['white','#df9a31'])
-
-const popDensity = scalePow()
-	.exponent(0.5)
-	.domain([0,20000])
-	.range(['white','#df9a31'])
-
 const sources = {
 	Toronto: {
 		DAs: require('./data/Toronto/da_polygons.topojson' ),
@@ -30,40 +20,71 @@ const sources = {
 }
 
 export default function(props){
+	// not yet supported
 	if(props.layer.name == 'Landuse'){ return null }
-	const [ boundary, setBoundary ] = useState(null)
-	const [ DAs, setDAs ] = useState({features:[]})
+	return (
+		<LayerGroup>
+			<CityBoundary city={props.city}/>
+			<DisseminationAreas city={props.city} layer={props.layer}/>
+		</LayerGroup>
+	)
+}
+
+function CityBoundary(props){
+	const [ boundaryFeature, setBoundaryFeature ] = useState(null)
+	useEffect(()=>{
+		const cityData = sources[props.city.name]
+		json(cityData.boundary).then( data => {
+			setBoundaryFeature( topo2geo(data,'csd_boundary').features[0] )
+		} )
+	},[props.city])
+	if( ! boundaryFeature ) return null;
+	console.log(boundaryFeature)
+	return (
+		<GeoJSON id="city-boundary"
+			data={boundaryFeature}
+			pathOptions={ {color:'black',fill:false, weight: 1} }/>
+	)
+}
+
+const empDensity = scalePow()
+	.exponent(0.5)
+	.domain([0,50000])
+	.range(['white','#df9a31'])
+
+const popDensity = scalePow()
+	.exponent(0.5)
+	.domain([0,20000])
+	.range(['white','#df9a31'])
+
+function DisseminationAreas(props){
+	const [ DAs, setDAs ] = useState(null)
 	useEffect(()=>{
 		const cityData = sources[props.city.name]
 		json(cityData.DAs).then( data => {
 			setDAs( topo2geo(data,'da_polygons') )
 		} )
-		json(cityData.boundary).then( data => {
-			setBoundary( topo2geo(data,'csd_boundary').features[0] )
-		} )
 	},[props.city])
+	if ( !DAs ) return null;
+	const [ fillFunc, fillProp ] = props.layer.name == 'Employment' ? 
+		[ empDensity, 'density_employment' ] : 
+		[ popDensity, 'density_population' ];
+	const staticOptions = { 
+		stroke:true, 
+		color: "white", 
+		weight:0.5, 
+		fillOpacity:0.666, 
+		opacity:0.666
+	}
 	return (
-		<LayerGroup>
-			{ boundary &&
-				<GeoJSON id="city-boundary"
-					data={boundary}
-					pathOptions={ {color:'black',fill:false, weight: 1} }/>
-			}
-			{
-				DAs.features.map( da => {
-					let fill
-					if(props.layer.name == 'Employment'){
-						fill = empDensity(da.properties.density_employment)
-					}else{
-						fill = popDensity(da.properties.density_population)
-					}
-					return (
-						<GeoJSON key={da.properties.dauid}
-							data={da}
-							pathOptions={ {stroke:true, fillColor: fill, color: "white", weight:0.5, fillOpacity:0.666, opacity:0.666} }/>
-					)
-				} )
-			}
-		</LayerGroup>
+		<LayerGroup>{
+			DAs.features.map( da => {
+				let fill = { fillColor: fillFunc(da.properties[fillProp]) }
+				return (
+					<GeoJSON key={da.properties.dauid} data={da}
+						pathOptions={ { ...staticOptions, ...fill } }/>
+				)
+			} )
+		}</LayerGroup>
 	)
 }
