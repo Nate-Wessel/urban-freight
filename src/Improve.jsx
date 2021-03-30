5a7167ddb326ec3b216843fad33620f9a131df24
Nate Wessel <bike756@gmail.com>
2021-03-29 23:14:56 -0400

    lazy load paradigm layers

---
+++ src/Improve.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function(props){
+	const { city } = props
+	return null
+}
--- src/OverLayer.jsx
+++ src/OverLayer.jsx
@@ -1,25 +1,17 @@
 import React, { lazy, Suspense } from 'react'
-import Avoid from './Avoid'
+const Avoid = lazy(()=>import('./Avoid'))
+const Shift = lazy(()=>import('./Shift'))
+const Improve = lazy(()=>import('./Improve'))
 
 export default function (props){
 	const { city, paradigm } = props
 	switch(paradigm){
 		case 'avoid':
-			return <Avoid city={city}/>
+			return <Suspense fallback={null}><Avoid city={city}/></Suspense>
 		case 'shift':
-			return <Shift city={city}/>
+			return <Suspense fallback={null}><Shift city={city}/></Suspense>
 		case 'improve': 
-			return <Improve city={city}/>
+			return <Suspense fallback={null}><Improve city={city}/></Suspense>
 	}
-	return null
-}
-
-
-function Shift(props){
-	const { city } = props
-	return null
-}
-function Improve(props){
-	const { city } = props
 	return null
 }
