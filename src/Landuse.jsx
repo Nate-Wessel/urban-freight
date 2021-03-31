c8549c53cc42092b604dce9aec538fa6d23f38f9
Nate Wessel <bike756@gmail.com>
2021-03-30 23:20:22 -0400

    fix landuse immutable prop issue

--- src/Landuse.jsx
+++ src/Landuse.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { TileLayer, Pane } from 'react-leaflet'
 
 export default function(props){
 	const { city } = props
+	const layer = useRef(null)
+	useEffect(()=>{
+		// url is immutable in react-leaflet
+		// need to reset it directly when city changes
+		if(!layer.current) return;
+		layer.current.setUrl(url(city))
+	},[city])
 	return (
 		<Pane name="landuse" style={{zIndex:430}}>
-			<TileLayer url={`https://jamaps.github.io/land-use-tiles//${city.name}/tiles/{z}/{x}/{y}.png`}/>
+			<TileLayer ref={layer} url={url(city)}/>
 		</Pane>
 	)
+}
+
+function url(city){
+	let src = 'https://jamaps.github.io/land-use-tiles'
+	return `${src}/${city.name}/tiles/{z}/{x}/{y}.png`
 }
