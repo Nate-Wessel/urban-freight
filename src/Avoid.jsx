7ac83c795ccf04349a4f5dcbcd0b1b1bda6369eb
Nate Wessel <bike756@gmail.com>
2021-03-31 20:22:45 -0400

    render pickup points

--- src/Avoid.jsx
+++ src/Avoid.jsx
@@ -3,27 +3,32 @@ import { json } from 'd3-fetch'
 import { feature as topo2geo } from 'topojson-client'
 import { CircleMarker, LayerGroup } from 'react-leaflet'
 import { GeoJSON } from 'leaflet'
+import { scaleOrdinal } from 'd3-scale'
+
+const color = scaleOrdinal()
+	.domain(['Purol','Fedex','UPS','Penguin'])
+	.range(['red','green','blue','black'])
 
 const data = {
-	Toronto: require('./data/Toronto/fedex.topojson'),
-	Edmonton: require('./data/Edmonton/fedex.topojson'),
-	Vancouver: require('./data/Vancouver/fedex.topojson')
+	Toronto: require('./data/Toronto/pickup_pts.topojson'),
+	Edmonton: require('./data/Edmonton/pickup_pts.topojson'),
+	Vancouver: require('./data/Vancouver/pickup_pts.topojson')
 }
 
 export default function(props){
 	const { city } = props
 	const [ points, setPoints ] = useState([])
 	useEffect(()=>{
-		json(data[city.name]).then( d => {
-			setPoints( topo2geo(d,'fedex').features ) 
+		json(data[city.name]).then( resp => {
+			setPoints( topo2geo(resp,'pickup_pts').features )
 		} )
 	},[city])
 	let pointFeatures = points.map( (feat,i) => {
 		let ll = GeoJSON.coordsToLatLng(feat.geometry.coordinates)
 		return ( 
 			<CircleMarker key={`fedex/${city.name}/${i}`} 
-				center={ll} 
-				pathOptions={{color:'red'}}/>
+				center={ll} radius={5}
+				pathOptions={{'color':color(feat.properties.type)}}/>
 		)
 	} )
 	return <LayerGroup>{pointFeatures}</LayerGroup>
