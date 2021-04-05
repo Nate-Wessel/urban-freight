85fd4452d5dcc69b6d2c46352065edfe924e938d
Nate Wessel <bike756@gmail.com>
2021-04-05 16:46:04 -0400

    fetch and render bikeshare stations

--- src/Shift/BikeShare.jsx
+++ src/Shift/BikeShare.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { CircleMarker, Tooltip } from 'react-leaflet'
+import { json } from 'd3-fetch'
+import { scalePow } from 'd3-scale'
+
+const data = {
+	Toronto: 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information',
+	Vancouver: 'https://vancouver-gbfs.smoove.pro/gbfs/en/station_information.json'
+}
+
+// area ~= to capacity
+const radius = scalePow().exponent(0.5).domain([5,40]).range([2,10])
 
 export default function(props){
 	const { city } = props
-	return null
+	const [ stations, setStations ] = useState([])
+	useEffect(()=>{
+		if(city.name in data){
+			json(data[city.name]).then( resp => {
+				setStations(resp.data.stations)
+			} )
+		}else{
+			setStations([])
+		}
+	},[city])
+	return stations.map( station => {
+		return (
+			<CircleMarker key={station.station_id}
+				center={[station.lat,station.lon]} 
+				radius={radius(station.capacity)}
+				pathOptions={{weight:1}}>
+				<Tooltip>
+					{`${station.name} - capacity: ${station.capacity}`}
+				</Tooltip>
+			</CircleMarker>
+		)
+	} )
 }
