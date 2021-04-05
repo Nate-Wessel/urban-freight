dc0f80ce5ef412a55facdfe65910f964278cb1bf
Nate Wessel <bike756@gmail.com>
2021-04-05 11:00:20 -0400

    add empty bike share component

--- src/Shift/index.jsx
+++ src/Shift/index.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { LayerGroup } from 'react-leaflet'
 import ParkingLots from './ParkingLots'
+import BikeShare from './BikeShare'
 
 export default function(props){
 	const { city, displayed } = props
 	
 	return (
 		<LayerGroup>
-			{displayed.has('parking-lots') &&
-				<ParkingLots city={city}/>
-			}
+			{displayed.has('parking-lots') && <ParkingLots city={city}/>}
+			{displayed.has('bike-share') && <BikeShare city={city}/>}
 		</LayerGroup>
 	)
 }
