5b311e795202cb54f36659d4058d4ea52f216746
Nate Wessel <bike756@gmail.com>
2021-04-11 20:22:19 -0400

    add EV efficiency points

--- src/FuelChart/index.jsx
+++ src/FuelChart/index.jsx
@@ -9,23 +9,29 @@ const [ width, height ] = [ 500, 200 ]
 const cities = [
 	{
 		label: 'Toronto',
-		diesel: 1.00108,
-		gasoline: 1.1138
+		diesel: 40.34,
+		gasoline: 29.24,
+		electricHDT: 17.87,
+		electricMDT: 2.60
 	},
 	{
 		label: 'Edmonton',
-		diesel: 0.9754,
-		gasoline: 1.0005
+		diesel: 40.88,
+		gasoline: 29.23,
+		electricHDT: 22.825,
+		electricMDT: 3.32
 	},
 	{
 		label: 'Vancouver',
-		diesel: 1.1449,
-		gasoline: 1.3642
+		diesel: 50.30,
+		gasoline: 35.81,
+		electricHDT: 17.325,
+		electricMDT: 2.52
 	},
 ]
 
 const chartX = scaleLinear()
-	.domain( [ 0, 1.6 ] )
+	.domain( [ 0, 50 ] )
 	.range( [ margin.l, width-margin.r ] )
 
 export default function(props){
@@ -45,12 +51,14 @@ function City(props){
 	let diesel = chartX(data.diesel)
 	let gas = chartX(data.gasoline)
 	return (
-		<g className="city" transform={`translate(0,${30+position*45})`}>
-			<text x={zero} y={0}>{data.label}</text>
+		<g className="city" transform={`translate(0,${30+position*50})`}>
+			<text x={zero} y={0}>{data.label}&nbsp;</text>
 			<path className="diesel" 
 				d={`M${zero},-11 L${diesel},-11 L${diesel},-1 L${zero},-1 z`}/>
+			<circle className="ev HDT" cx={chartX(data.electricHDT)} cy={-6} r="5"/>
 			<path className="gasoline" 
 				d={`M${zero},11 L${gas},11 L${gas},1 L${zero},1 z`}/>
+			<circle className="ev MDT" cx={chartX(data.electricMDT)} cy={6} r="5"/>
 		</g>
 	)
 }
@@ -67,7 +75,7 @@ function XAxis(props){
 	} )
 	return (
 		<g id="x-axis" transform={`translate(0,${ymin})`}>
-			<path d={`M${chartX(0)},0 L${chartX(1.6)},0`}/>
+			<path d={`M${chartX(0)},0 L${chartX(50)},0`}/>
 			{ticks}
 		</g>
 	)
