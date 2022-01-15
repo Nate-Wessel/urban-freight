028572e56fa0b791d21f7c70096ced89f8684e06
Nate Wessel <bike756@gmail.com>
2022-01-15 10:00:44 -0500

    rename boundary files

--- src/BaseLayer/CityBoundary.jsx
+++ src/BaseLayer/CityBoundary.jsx
@@ -5,9 +5,10 @@ import { geojson2leaflet } from '../geojson2leaflet'
 import { json } from 'd3-fetch'
 
 const sources = {
-	Toronto: require('../data/Toronto/csd_boundary.topojson'),
-	Vancouver: require('../data/Vancouver/csd_boundary.topojson'),
-	Edmonton: require('../data/Edmonton/csd_boundary.topojson')
+	Toronto: require('../data/Toronto/boundary.topojson'),
+	Vancouver: require('../data/Vancouver/boundary.topojson'),
+	Edmonton: require('../data/Edmonton/boundary.topojson'),
+	Ottawa: require('../data/Ottawa/boundary.topojson')
 }
 
 const style = {
@@ -17,13 +18,13 @@ const style = {
 	dashArray: '5 10 5'
 }
 
-export default function(props){
+export default function({city}){
 	const [ boundaryFeature, setBoundaryFeature ] = useState(null)
 	useEffect(()=>{
-		json(sources[props.city.name]).then( data => {
-			setBoundaryFeature( topo2geo(data,'csd_boundary').features[0] )
+		json(sources[city.name]).then( data => {
+			setBoundaryFeature( topo2geo(data,'boundary').features[0] )
 		} )
-	},[props.city])
+	},[city])
 	if( ! boundaryFeature ) return null;
 	let latLngs = geojson2leaflet(boundaryFeature.geometry)
 	return <Polygon id="city-boundary" positions={latLngs} pathOptions={style}/>
