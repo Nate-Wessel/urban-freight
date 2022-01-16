13bc4f79cfee8e02c7dd39480ec4e876fdcfbe2e
Nate Wessel <bike756@gmail.com>
2022-01-16 10:11:24 -0500

    add all new cities with boundary data

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,7 +1,12 @@
+import Calgary from './Calgary/data.js'
+import Edmonton from './Edmonton/data.js'
+import Halifax from './Halifax/data.js'
+import Hamilton from './Hamilton/data.js'
+import Ottawa from './Ottawa/data.js'
 import Toronto from './Toronto/data.js'
 import Vancouver from './Vancouver/data.js'
-import Edmonton from './Edmonton/data.js'
-import Ottawa from './Ottawa/data.js'
+import Victoria from './Victoria/data.js'
+import Winnipeg from './Winnipeg/data.js'
 
 export const cities = [
 	{
@@ -25,7 +30,37 @@ export const cities = [
 	{
 		name: 'Ottawa',
 		bounds: [[45.619,-76.434],[44.9,-75.154]],
-		geohashSqM: 16890, // TODO what is this variable??
+		geohashSqM: 1, // TODO do we really need this variable?
 		data: Ottawa
-	}
+	},
+	{
+		name: 'Calgary',
+		bounds: [[51.2230,-114.3538],[50.84257,-113.81025]],
+		geohashSqM: 1,
+		data: Calgary
+	},
+	{
+		name: 'Halifax',
+		bounds: [[44.5811,-63.72265],[44.71121,-63.54319]],
+		geohashSqM: 1,
+		data: Halifax
+	},
+	{
+		name: 'Hamilton',
+		bounds: [[43.0505,-80.2485],[43.4706,-79.6221]],
+		geohashSqM: 1,
+		data: Hamilton
+	},
+	{
+		name: 'Victoria',
+		bounds: [[48.4028,-123.398],[48.4504,-123.3224]],
+		geohashSqM: 1,
+		data: Victoria
+	},
+	{
+		name: 'Winnipeg',
+		bounds: [[49.713,-97.349],[49.994,-96.956]],
+		geohashSqM: 1,
+		data: Winnipeg
+	},
 ]
