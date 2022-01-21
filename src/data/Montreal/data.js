cf6a600a645df9c688522bde9e78c47a548aa935
Nate Wessel <bike756@gmail.com>
2022-01-20 20:20:17 -0500

    add boundary

--- src/data/Montreal/data.js
+++ src/data/Montreal/data.js
@@ -1,8 +1,8 @@
 export default {
-//	base: {
-//		boundary: require('./boundary.topojson'),
-//		tiles: { transit: false, landuse: false }
-//	},
+	base: {
+		boundary: require('./boundary.topojson'),
+		tiles: { transit: false, landuse: false }
+	},
 	shift: {
 		bikeShare: 'https://gbfs.velobixi.com/gbfs/en/station_information.json',
 	},
